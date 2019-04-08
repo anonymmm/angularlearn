af73a9080750afc5592ad57a638a0045754c2a24
anonymmm <adnanbutt0676@gmail.com>
2019-04-08 17:26:38 +0500

    code updated

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,20 @@ import {Component } from '@angular/core'
 })
 export class ProductListComponent{
     pageTitle: string = 'Product List';
+
+    //simple defination 
+
+private demoObject()
+{
+    let object = {
+        name:"Danyal",
+        phone:"03005151588"
+    }
+
+    console.log("object info",object.name);
+    
+}
+
+
+
 }
